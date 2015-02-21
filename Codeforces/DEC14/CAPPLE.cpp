5df7cfb8d947ba95f9f1f8ac6cdd6d803ee1aeed
infinitum-rakesh <rakeshnitcalicut@gmail.com>
2015-02-21 21:14:44 +0530

    New contests added

---
+++ Codeforces/DEC14/CAPPLE.cpp
@@ -0,0 +1,59 @@
+/***********************************************************************/
+//  																		
+//	INFINITUM 					                                    	
+//	Rakesh Mahadasa														
+//	National Institute of Technology , Calicut						
+//	problem link : 
+//	Concept : 
+//	Reference :	
+//																		
+/***********************************************************************/
+
+
+//header files
+
+#include <iostream>
+#include <string.h>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <cstdio>
+#include <iomanip>
+#include <cmath>
+#include <set>
+#include <map>
+#include <ctime>
+#include <limits.h>
+
+//typedefs
+
+typedef unsigned long long int ull;
+typedef long long int lli;
+typedef long int li;
+
+#define MOD7 1000000007
+#define MOD9 1000000009
+
+using namespace std;
+
+
+
+
+int main(){
+	int t;
+	scanf("%d",&t);
+	while(t--){
+		int n;
+		cin>>n;
+		int maxi = -1;
+		int mini = 1000001;
+		for(int i =0; i < n;i++){
+			int temp;
+			cin>>temp;
+			ifb
+
+		}
+	}
+	return 0;
+}
