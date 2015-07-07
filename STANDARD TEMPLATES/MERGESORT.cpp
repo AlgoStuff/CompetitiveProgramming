a64b496132c8b11976d6a823051a394530f2786d
Rakesh M <rakeshnitcalicut@gmail.com>
2015-07-07 12:09:15 +0530

    Adding standard templates. sorting

---
+++ STANDARD TEMPLATES/MERGESORT.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+
+using namespace std;
+
+
+void merge(int a[],int start,int mid,int end){
+	int left = mid-start+1;
+	int right = end-mid;
+	int la[left],ra[right];
+	for(int i = start; i <=mid;i++){
+		la[i-start] = a[i];
+	}
+	for(int i = mid+1;i <=end;i++){
+		ra[i-mid-1] = a[i];
+	}
+	int lc = 0,rc = 0,mc=start;
+	while(lc < left && rc < right){
+		if(la[lc]<ra[rc]){
+			a[mc]=la[lc];
+			lc++;
+		}
+		else{
+			a[mc]=ra[rc];
+			rc++;
+		}
+		mc++;
+	}
+
+	while(lc<left){
+		a[mc]=la[lc];
+		lc++;
+		mc++;
+	}
+	while(rc<right){
+		a[mc]=ra[rc];
+		rc++;
+		mc++;
+	}
+}
+
+void mergesort(int a[],int start,int end){
+	if(start<end){
+		int mid = (start+end)/2;
+		mergesort(a,start,mid);
+		mergesort(a,mid+1,end);
+		merge(a,start,mid,end);
+	}
+}
+
+
+int main(){
+	int n;
+	cin>>n;
+	int a[n];
+	for(int i = 0; i < n;i++){
+		cin>>a[i];
+	}
+	mergesort(a,0,n-1);
+	for(int i = 0; i < n;i++){
+		cout<<a[i]<<" ";
+	}
+	cout<<endl;
+}
---
+++ STANDARD TEMPLATES/SELECTIONSORT.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int n;
+	cin>>n;
+	int a[n];
+	for(int i = 0; i < n;i++){
+		cin>>a[i];
+	}
+
+	for(int i = 0; i < n;i++){
+		int min = a[i];
+		int minpos = i;
+		for(int j = i; j < n;j++){
+			if(a[j]<min){
+				min = a[j];
+				minpos = j;
+			}
+		}
+		int temp = a[i];
+		a[i] = a[minpos];
+		a[minpos] = temp;
+	}
+
+	for(int i =0; i < n;i++){
+		cout<<a[i]<<" ";
+	}
+	cout<<endl;
+}
